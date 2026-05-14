5a721562f9fc3f35b4868e5f0885b9b6de6e62ae
gopika <gopikasolanki6@gmail.com>
2026-05-14 13:57:05 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,7 @@ const {isLoggedIn, isOwner} = require("../middleware.js")
 const {validateListing} = require("../middleware.js");
 const listingController = require("../controllers/listings.js");
 const multer  = require('multer');
-const upload = multer({ storage });
+const upload = multer({ dest: "uploads/" });
 
 router.route("/")    
     .get(wrapAsync(listingController.index))   //index route
